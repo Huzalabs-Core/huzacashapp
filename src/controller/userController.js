dc15c738ca87fd8c7ca74c2bedae6cf54e358135
Elyse350 <mutonielyse35@gmail.com>
2022-06-17 14:56:07 +0200

    addition get function

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -42,7 +42,15 @@ class userController {
       })
     }
     
-  
+    static async getAllUsers(req,res){
+      const users= await userInfos.find();
+      if(!users){
+          return res.status(404).json({error:"no users registered"})
+      }
+      return res
+      .status(200)
+      .json({message:"Successfully retrieved users" , data: users});
+  }
 }
 
 export default userController;
